adebebd7144864fc53827489149b043dabb833f2
zampralight <zampralight@gmail.com>
2020-05-11 05:24:53 -0400

    initialized the project

---
+++ nuxt.config.js
@@ -0,0 +1,40 @@
+const colors = require('vuetify/es5/util/colors').default
+
+module.exports = {
+  mode: 'universal',
+  head: {
+    titleTemplate: '%s - ' + process.env.npm_package_name,
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  loading: { color: '#fff' },
+  css: [
+  ],
+  plugins: [
+  ],
+  buildModules: [
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/vuetify'
+  ],
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    '@nuxtjs/dotenv'
+  ],
+  axios: {
+  },
+  vuetify: {
+    customVariables: ['~/assets/variables.scss']
+  },
+  build: {
+    extend (config, ctx) {
+    }
+  }
+}
